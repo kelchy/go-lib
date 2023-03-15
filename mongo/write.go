5c2789cda01eb6512ed1d9960e6e34194b8f6264
Zhenhao <zhenhao.chen+git@circles.asia>
2023-03-15 15:44:10 +0800

    fix: add bulkwrite method

--- mongo/write.go
+++ mongo/write.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
@@ -93,13 +94,12 @@ func (client Client) UpdateMany(ctx context.Context, colname string, filter inte
 }
 
 // UpdateOne - function to update a single doc in the collection, ctx can be nil
-func (client Client) UpdateOne(ctx context.Context, colname string,
-	filter interface{}, update interface{}, opts *options.UpdateOptions) (int64, error) {
+func (client Client) UpdateOne(ctx context.Context, colname string, filter interface{}, update interface{}, opts ...*options.UpdateOptions) (int64, error) {
 	// select collection
 	col := client.Db.Collection(colname)
 
 	// update
-	result, e := col.UpdateOne(ctx, filter, update, opts)
+	result, e := col.UpdateOne(ctx, filter, update, opts...)
 	if e != nil {
 		client.log.Error("MONGO_UPDATEONE", e)
 		return 0, e
@@ -148,4 +148,20 @@ func (client Client) ReplaceOne(ctx context.Context, colname string,
 		return 0, e
 	}
 	return result.ModifiedCount, e
+}
+
+// BulkWrite - function to write on multiple documents
+// ctx can be nil, opts is operations to execute on the collection
+func (client Client) BulkWrite(ctx context.Context, collname string, models []mongo.WriteModel, opts ...*options.BulkWriteOptions) (mongo.BulkWriteResult, error) {
+
+	// select the collection
+	coll := client.Db.Collection(collname)
+
+	result, err := coll.BulkWrite(ctx, models, opts...)
+	if err != nil {
+		client.log.Error("MONGO_BULKWRITE", err)
+		return mongo.BulkWriteResult{}, err // return nil struct for err
+	}
+
+	return *result, nil
 }
